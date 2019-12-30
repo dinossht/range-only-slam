c20a7b58fb16a887765a62198a2fa100cc652e06
Dinosshan Thiagarajah <dinosshan1996@hotmail.com>
2019-12-30 19:02:58 +0100

    change from 30 to 45 deg

--- listener.py
+++ listener.py
@@ -5,44 +5,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-s = 200
+s = 500
 map2d = np.zeros((s+1, s+1))
 x_off = s/2
 y_off = s/2
 mx = x_off
 my = y_off
 
+win = 50
+
 pub = rospy.Publisher('ultrasound', Int32, queue_size=10)
 def callback(data):
     global mx, my
     x = data.data.split()
-    print(int(x[0]))
-    print(int(x[1]))
-    print(int(x[2]))
-    print("        ")
     pub.publish(int(x[2]))
 
-    mx = mx + round(int(x[0]) * np.sin(np.pi * int(x[1]) / 180))
-    my = my - round(int(x[0]) * np.cos(np.pi * int(x[1]) / 180))
+    mx = int(mx + round(int(x[0]) * np.sin(np.pi * int(x[1]) / 180)))
+    my = int(my - round(int(x[0]) * np.cos(np.pi * int(x[1]) / 180)))
 
-    print(mx)
-    print(my)
-    print("        ")
-    #map2d[my][mx] = 150
-
-    if int(x[2]) >= 20 and int(x[2]) <= 50:
-        tx = mx + round(int(x[2]) * np.sin(np.pi * int(x[1]) / 180))
-        ty = my - round(int(x[2]) * np.cos(np.pi * int(x[1]) / 180))
+    map2d[my][mx] = 150
+    sonar = int(x[2])
+    if sonar >= 10 and sonar <= 80:
+        tx = mx + round(sonar * np.sin(np.pi * int(x[1]) / 180))
+        ty = my - round(sonar * np.cos(np.pi * int(x[1]) / 180))
         map2d[ty][tx] = 255
 
     plt.style.use('classic')
     plt.imshow(map2d, cmap='gray')
+    plt.xlim((mx-win, mx+win))
+    plt.ylim((my-win, my+win))
     plt.pause(0.1)
     map2d[my][mx] = 0#100
-    if int(x[2]) >= 20 and int(x[2]) <= 50:
+    if sonar >= 10 and sonar <= 80:
         tx = mx + round(int(x[2]) * np.sin(np.pi * int(x[1]) / 180))
         ty = my - round(int(x[2]) * np.cos(np.pi * int(x[1]) / 180))
-        map2d[ty][tx] = 230
+        map2d[ty][tx] = 200
 
 
 
